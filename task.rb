2e800f818ebc272de9570324495211ccd7346ccc
健太 <kenta@mba.local>
2020-03-31 22:16:46 +0900

    Q11-16

--- task.rb
+++ task.rb
@@ -88,14 +88,16 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten.uniq.each.with_index(1){|sport,i|
+      puts "No#{i} #{sport}"
+  }
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -103,14 +105,20 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data[:age] =  update_data[:age]
+  user_data[:address] = update_data[:address]
+  p user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  array = []
+  data.each_key{|key|
+      array << key
+  }
+  p array
 end
 
 def q15
@@ -118,7 +126,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  data1.has_key?(:age) ? (puts "OK") : (puts "NG")
+  data2.has_key?(:age) ? (puts "OK") : (puts "NG")
 end
 
 def q16
